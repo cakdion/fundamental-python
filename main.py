7a055cf014d3aa9aef2864c4e5bd0aae878f9d50
cakdion <ajatotindo@gmail.com>
2020-10-03 21:02:52 +0700

    intial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+# KONSTRUKSI DASAR PYTHON
+# SEQUENTIAL: Eksekusi berurutan
+print('Hello World!')
+print('bay Cakdion')
+print('03 Oktober 2020')
+print('-' * 15)
+
+# PERCABANGAN : Ekesekusi terpilih
+ingin_cepat = True
+if ingin_cepat:
+    print('Jalan lurus aja, ya!')
+else:
+    print('Jalan lain')
+
+# PERCABANGAN PILIHAN LEBIH DARI 2
+ingin_balik = True
+
+# PERULANGAN
+jumlah_anak = 4
+
+for index_anak in range(1, jumlah_anak + 1):
+    print(f'Halo anak {index_anak}')
